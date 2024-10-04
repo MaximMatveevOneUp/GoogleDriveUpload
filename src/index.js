47550b7ae9984dacf4763971c642dea555ca415d
Maxim Matveev <129727979+MaximMatveevOneUp@users.noreply.github.com>
2024-10-04 11:10:04 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,118 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+
+const actions = require('@actions/core');
+const { google } = require('googleapis');
+
+const credentials = actions.getInput('credentials', { required: true });
+const parentFolderId = actions.getInput('parent_folder_id', { required: true });
+const target = actions.getInput('target', { required: true });
+const owner = actions.getInput('owner', { required: false });
+const childFolder = actions.getInput('child_folder', { required: false });
+const overwrite = actions.getInput('overwrite', { required: false }) === 'true';
+let filename = actions.getInput('name', { required: false });
+
+const credentialsJSON = JSON.parse(Buffer.from(credentials, 'base64').toString());
+const scopes = ['https://www.googleapis.com/auth/drive.file'];
+const auth = new google.auth
+    .JWT(credentialsJSON.client_email, null, credentialsJSON.private_key, scopes, owner);
+const drive = google.drive({ version: 'v3', auth });
+
+async function getUploadFolderId() {
+    if (!childFolder) {
+        return parentFolderId;
+    }
+
+    // Check if child folder already exists and is unique
+    const { data: { files } } = await drive.files.list({
+        q: `name='${childFolder}' and '${parentFolderId}' in parents and trashed=false`,
+        fields: 'files(id)',
+        includeItemsFromAllDrives: true,
+        supportsAllDrives: true,
+    });
+
+    if (files.length > 1) {
+        throw new Error('More than one entry match the child folder name');
+    }
+    if (files.length === 1) {
+        return files[0].id;
+    }
+
+    const childFolderMetadata = {
+        name: childFolder,
+        mimeType: 'application/vnd.google-apps.folder',
+        parents: [parentFolderId],
+    };
+    const { data: { id: childFolderId } } = await drive.files.create({
+        resource: childFolderMetadata,
+        fields: 'id',
+        supportsAllDrives: true,
+    });
+
+    return childFolderId;
+}
+
+async function getFileId(targetFilename, folderId) {
+    const { data: { files } } = await drive.files.list({
+        q: `name='${targetFilename}' and '${folderId}' in parents`,
+        fields: 'files(id)',
+    });
+
+    if (files.length > 1) {
+        throw new Error('More than one entry match the file name');
+    }
+    if (files.length === 1) {
+        return files[0].id;
+    }
+
+    return null;
+}
+
+async function main() {
+    const uploadFolderId = await getUploadFolderId();
+
+    if (!filename) {
+        filename = target.split('/').pop();
+    }
+
+    let fileId = null;
+
+    if (overwrite) {
+        fileId = await getFileId(filename, uploadFolderId);
+    }
+
+    const fileData = {
+        body: fs.createReadStream(target),
+    };
+
+    if (fileId === null) {
+        if (overwrite) {
+            actions.info(`File ${filename} does not exist yet. Creating it.`);
+        } else {
+            actions.info(`Creating file ${filename}.`);
+        }
+        const fileMetadata = {
+            name: filename,
+            parents: [uploadFolderId],
+        };
+
+        drive.files.create({
+            resource: fileMetadata,
+            media: fileData,
+            uploadType: 'multipart',
+            fields: 'id',
+            supportsAllDrives: true,
+        });
+    } else {
+        actions.info(`File ${filename} already exists. Updating it.`);
+        drive.files.update({
+            fileId,
+            media: fileData,
+        });
+    }
+    actions.info(`Getting fileId`);
+    fileId = await getFileId(filename, uploadFolderId);
+    actions.setOutput('fileId', fileId);
+}
+
+main().catch((error) => actions.setFailed(error));
